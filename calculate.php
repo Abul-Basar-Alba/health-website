147f227021098172bd4f222b16dc0a9e33ed19e0
Abul_Basar <mdabulbasarbasar025@gmail.com>
2025-03-16 00:51:46 +0600

    U

--- calculate.php
+++ calculate.php
@@ -9,6 +9,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST')
         echo json_encode(['error' => 'Invalid weight value']);
         exit;
     }
+
     
     $weight = floatval($_POST['weight']);
     
@@ -60,9 +61,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST')
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nutrition Calculator</title>
+    
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="../assets/css/style.css">
+    <script src="assets/js/calculate.js" defer></script>
+    <script src="../assets/js/calculate.js" defer></script>
+
 </head>
+
 <body>
 
 </body>
