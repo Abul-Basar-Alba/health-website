0735039e0d8c5c9e48ff6e94e2d2a65189e25d9f
Abul_Basar <mdabulbasarbasar025@gmail.com>
2025-03-25 23:53:21 +0600

    U

--- signup.php
+++ signup.php
@@ -1,25 +1,27 @@
 <?php include 'includes/header.php'; ?>
 
-<div class="signup-container">
-    <h1>Sign Up</h1>
-    <form action="signup_process.php" method="POST">
-        <div class="form-group">
-            <label for="username">Username</label>
-            <input type="text" id="username" name="username" required class="form-control">
-        </div>
-        <div class="form-group">
-            <label for="email">Email</label>
-            <input type="email" id="email" name="email" required class="form-control">
-        </div>
-        <div class="form-group">
-            <label for="password">Password</label>
-            <input type="password" id="password" name="password" required class="form-control">
-        </div>
-        <button type="submit" class="btn btn-primary">Sign Up</button>
-    </form>
-    <p>Already have an account? <a href="login.php">Login here</a></p>
-</div>
-<link rel="stylesheet" href="assets/css/style.css">
+<body class="signup-page">
+    <div class="signup-container">
+        <h1>Sign Up</h1>
+        <form action="signup_process.php" method="POST">
+            <div class="form-group">
+                <label for="username">Username</label>
+                <input type="text" id="username" name="username" required class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" id="email" name="email" required class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password" required class="form-control">
+            </div>
+            <button type="submit" class="btn btn-primary">Sign Up</button>
+        </form>
+        <p>Already have an account? <a href="login.php">Login here</a></p>
+    </div>
+    <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="../assets/css/style.css">
+</body>
 
 <?php include 'includes/footer.php'; ?>
